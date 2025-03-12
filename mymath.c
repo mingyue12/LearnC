27d41a35f95ab26aba0f2c20bb8c0d0ba635ff2b
mingyue12 <108075525+mingyue12@users.noreply.github.com>
2025-03-13 00:31:36 +0800

    重构

--- mymath.c
+++ mymath.c
@@ -45,9 +45,77 @@ int Fib(int n)	//斐波那契数列
 	}*/ //递归实现
 }
 
-void Swap(int* a, int* b)
+void Swap(int* a, int* b)	//交换两个数
 {
 	int temp = *a;
 	*a = *b;
 	*b = temp;
+}
+
+int fac(int n)	//求阶乘
+{
+	if (n <= 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return n * fac(n - 1);
+	}
+}
+
+int my_strlen(char* str)	//计算字符串长度
+{
+	int count = 0;
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return count;
+}
+
+bool is_year(int year) // 判断是不是闰年 规则1：年份能被4整除但不能被100整除；规则2：年份能被400整除
+{
+	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+int binary_search(int arr[], int n, int key) // 二分查找
+{
+	int left = 0;
+	int right = n - 1;
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+		if (arr[mid] == key)
+		{
+			return mid;
+		}
+		else if (arr[mid] < key)
+		{
+			left = mid + 1;
+		}
+		else
+		{
+			right = mid - 1;
+		}
+	}
+
+	return -1;
+}
+
+void print_int(int num)	//递归打印整数的每一位
+{
+	if (num > 9)
+	{
+		print(num / 10);
+	}
+	printf("%d ", num % 10);
 }
--- mymath.h
+++ mymath.h
@@ -7,3 +7,12 @@ int Fib(int n);	//斐波那契数列
 
 void Swap(int* a, int* b);	//交换两个数
 
+int fac(int n);   //求阶乘
+
+int my_strlen(char* str);	//计算字符串长度
+
+bool is_year(int year); // 判断是不是闰年 规则1：年份能被4整除但不能被100整除；规则2：年份能被400整除
+
+int binary_search(int arr[], int n, int key); // 二分查找
+
+void print_int(int num);	//递归打印整数的每一位
